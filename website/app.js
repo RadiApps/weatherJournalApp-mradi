4273c439ff178b8248f63f33549bcdd565f5d96e
RadiApps <radysoft2013@gmail.com>
2021-04-10 13:09:12 +0300

    change to promises

--- website/app.js
+++ website/app.js
@@ -4,17 +4,20 @@
     // Create a new date instance dynamically with JS
     let d = new Date();
     let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+    //my api key for openweatherapi
     const apikey='67b02d488eb01f72648a43f2458f089b';
 
+    //selecting button from the html
     const button=document.querySelectorAll("#generate");
-    button[0].addEventListener("click",getWeatherData);
 
+    //fire click event for getting the tempruture
+    button[0].addEventListener("click",process);
+
+    //function fetching the temp from the remote api
     async function getWeatherData(){
         try{
             const zipCode = document.getElementById("zip").value;
-            const feelings = document.getElementById("feelings").value;
-
-            debugger;
             if(zipCode == '' || zipCode == undefined){
                 alert("Pleas enter zipcode first");
                 return;
@@ -22,23 +25,7 @@
             const url=`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apikey}&units=metric`;
             const response= await fetch(url);
             const data=  await response.json();
-            
-            await fetch('/addWeather',{
-                method:"POST",
-                credntials: "same-origin",
-                headers:{"Content-Type":"application/json"},
-                body:JSON.stringify({
-                    date : newDate,
-                    temp : data.main.temp,
-                    feelings:feelings
-                })
-            })
-            debugger;
-            const res=await fetch("/getWeather",{method: "GET",credntials: "same-origin"});
-            const resData =await res.json();
-            
-            updateHTML(resData);
-
+            return data.main.temp;
         }catch(err){
             console.log(err);
         }
@@ -49,3 +36,24 @@
         document.getElementById("temp").innerHTML='Temp is :'+resData.temp;
         document.getElementById("content").innerHTML='Date is :'+resData.feelings;
     }
+    function process(){
+        getWeatherData().then(temp=>{
+            const feelings = document.getElementById("feelings").value;
+            fetch('/addWeather',{
+                method:"POST",
+                credntials: "same-origin",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify({
+                    date : newDate,
+                    temp : temp,
+                    feelings:feelings
+                })
+            });
+            const res=  fetch("/getWeather",{method: "GET",credntials: "same-origin"});
+            const resData =  res;
+            return resData;
+
+        }).then(resData => {
+            updateHTML(resData);
+        }).catch(error=>console.log(error));
+    }      
